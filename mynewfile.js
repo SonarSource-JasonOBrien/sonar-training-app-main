dfd1fed33b0dfcfcb3f4482a4090ec80954ce330
Jason O'Brien <jasonobrien@edgar.lan>
2022-06-03 12:14:38 -0500

    Hotspot

--- mynewfile.js
+++ mynewfile.js
@@ -7,5 +7,14 @@ function (req, res) {
   let data = fs.readFileSync(reqPath, { encoding: 'utf8', flag: 'r' }); 
 }
 
+ip = "192.168.12.42"; 
+
+const net = require('net');
+var client = new net.Socket();
+client.connect(80, ip, function() {
+  // ...
+});
+
+
 let token = jwt.sign({ foo: 'bar' }, key, { algorithm: 'none' }); 
 jwt.verify(token, key, { expiresIn: 360000 * 5, algorithms: ['RS256', 'none'] }, callbackcheck);
